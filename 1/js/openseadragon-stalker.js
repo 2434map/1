6cb848473bbba5d0e64f0757985529660eda31f3
ujoutr <penguin@inpane.com>
2020-10-09 13:41:33 +0900

    up

--- 1/js/openseadragon-stalker.js
+++ 1/js/openseadragon-stalker.js
@@ -23,21 +23,23 @@
 		im.innerHTML = '<img src="images/skin80.gif" alt="" width="80" height="80">';
 		body.appendChild(im);
 
-		var tracker = new OpenSeadragon.MouseTracker({
-			element: self.container, 
-			moveHandler: mousemove
-		});
-		tracker.setTracking(true);
+		window.addEventListener('mouseover',
+		function(e){
+			im.style.left = e.clientX + 10 + "px"; //横の位置
+			im.style.top  = e.clientY + 10 + "px"; //縦の位置
+		}, false);
 
 		function mousemove(e){
 			im.style.left = e.position.x + imX + "px"; //横の位置
 			im.style.top  = e.position.y + imY + "px"; //縦の位置
 		}
 
-		self.addHandler('canvas-drag', function(e) {
-			im.style.left = e.position.x + imX + "px"; //横の位置
-			im.style.top  = e.position.y + imY + "px"; //縦の位置
+		var tracker = new OpenSeadragon.MouseTracker({
+			element: self.container, 
+			moveHandler: mousemove
 		});
+		tracker.setTracking(true);
+		self.addHandler('canvas-drag', mousemove);
 	};
 
 })();
